8ab9da18cd6f170f57dbd7ecf6fe08044dd322e6
Laith <Laith@DESKTOP-LAITH>
2023-04-20 00:56:57 +0300

    Add project files.

---
+++ WebApplicationGitHubDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationGitHubDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
